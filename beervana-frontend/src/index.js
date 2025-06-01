4f511aa8d4ec4584f978226e9a3015a8382f5e03
Matiaseze <matyaseze777@gmail.com>
2025-06-01 16:30:33 -0300

    Dockerfile y docker compose para poder ejecutar el front local. Proyecto de React limpio

---
+++ beervana-frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap 5.3
+import './index.css'; // Para mantener estilos globales
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
