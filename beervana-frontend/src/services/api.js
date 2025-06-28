5441a348a7c78e5538486ef2f388f327fa4dc38b
Matiaseze <matyaseze777@gmail.com>
2025-06-28 11:08:59 -0300

    FastFix: cambio a la url de produccion

--- beervana-frontend/src/services/api.js
+++ beervana-frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Usá el dominio real si está en producción
 const api = axios.create({
-
-    baseURL: 'https://beervana-4i1bzpr9o-rodrigos-projects-fa5acc60.vercel.app/my_api', // Test: desde la preview en vercel
+  baseURL: 'https://beervana.vercel.app/my_api'
+    /* baseURL: 'https://beervana-4i1bzpr9o-rodrigos-projects-fa5acc60.vercel.app/my_api', // Test: desde la preview en vercel */
     /* baseURL: 'http://localhost:8000/my_api', // prefijo personalizado */
 });
 
